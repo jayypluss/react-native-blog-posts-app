cd74fafeed80673a434824783389bbfe31f2eacc
Jonathan F. K <jonathanfk1@gmail.com>
2021-02-14 17:23:30 -0300

    Final study commit.

--- src/api/jsonServer.js
+++ src/api/jsonServer.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://57df2b07e071.ngrok.io'
+    baseURL: 'http://7008816acf1c.ngrok.io'
 });
--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -34,21 +34,27 @@ const getBlogPosts = dispatch => {
 };
 
 const addBlogPost = dispatch => {
-    return (title, content, callback) => {
-        dispatch({ type: 'add_blogpost', payload: {title, content} });
+    return async (title, content, callback) => {
+        await jsonServer.post('blogposts', {title, content});
         if (callback) callback();
+        
+        // dispatch({ type: 'add_blogpost', payload: {title, content} });
     };
 };
 
 const editBlogPost = dispatch => {
-    return (id, title, content, callback) => {
+    return async (id, title, content, callback) => {
+        await jsonServer.put(`/blogposts/${id}`, {title, content});
+
         dispatch({ type: 'edit_blogpost', payload: {id, title, content} });
         if (callback) callback();
     };
 };
 
 const deleteBlogPost = dispatch => {
-    return (id) => {
+    return async (id) => {
+        await jsonServer.delete(`/blogposts/${id}`);
+
         dispatch({ type: 'delete_blogpost', payload: id });
     };
 };
